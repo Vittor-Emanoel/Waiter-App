1da7417806697f8d7c0c4958a4460ee57d4713cf
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-07-26 01:15:40 -0300

    feat: create home page sections mobile

--- app/src/Main/index.tsx
+++ app/src/Main/index.tsx
@@ -1,11 +1,27 @@
-import { Container } from "./styles";
+import {
+  Container,
+  CategoriesContainer,
+  MenuContainer,
+  Footer,
+  FooterContainer,
+} from "./styles";
 
 import { Header } from "../components/Header";
 
 export function Main() {
   return (
-    <Container>
-      <Header />
-    </Container>
+    <>
+      <Container>
+        <Header />
+
+        <CategoriesContainer></CategoriesContainer>
+
+        <MenuContainer></MenuContainer>
+      </Container>
+
+      <Footer>
+        <FooterContainer></FooterContainer>
+      </Footer>
+    </>
   );
 }
